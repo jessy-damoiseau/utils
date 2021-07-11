322aa3e11e8c2977294aec3af970cee43531009a
jessy-damoiseau <jessydamoiseau@gmail.com>
2021-07-12 00:25:27 +0200

    modif script

--- scriptvps.sh
+++ scriptvps.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd ~
 touch .vimrc
-mkdir servers
 echo "syntax on" > .vimrc
 echo "set nu" >> .vimrc
 echo "set mouse=a" >> .vimrc
@@ -14,5 +13,6 @@ sudo apt install default-jdk
 sudo apt install git
 git config --global user.name "jessy-damoiseau"
 git config --global user.email "jessydamoiseau@gmail.com"
+git clone https://github.com/jessy-damoiseau/servers.git
 ssh-keygen
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
